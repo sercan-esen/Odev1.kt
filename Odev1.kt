cdb7da2c041cf6fbb468211f9003eaca2d7e3294
SERCAN ESEN <99819569+sercan-esen@users.noreply.github.com>
2022-06-14 13:38:15 +0300

    Add files via upload

---
+++ Odev1.kt
@@ -0,0 +1,26 @@
+package com.sercanesen.kotlindersleri.degiskenler
+
+
+fun main() {
+    val islemNo = 345
+    val ogrNo = 239
+    val kitapNo = 35
+    val aTarih = "2022/6/12"
+    val vTarih = "2022/6/14"
+    val ogrAd = "Sercan"
+    val ogrSoyad = "Esen"
+    val cinsiyet = "Erkek"
+    val dTarih = "2022/6/13"
+    val sinif = "4.sınıf"
+    val isbnNo = 9789756856009
+    val kitapAdi = "Sefiller"
+    val yazarNo = 27
+    val turNo = 3
+    val sayfaSayisi = 400
+    val puan = 300
+    val yazarAd = "Victor"
+    val yazarSoyad = "Hugo"
+    val turAdi = "Roman"
+
+
+}
